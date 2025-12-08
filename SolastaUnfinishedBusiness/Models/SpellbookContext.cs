b97ccaec136d69ac9ec805aeb141c288d27b9144
andargor@yahoo.com <andargor@yahoo.com>
2025-12-08 16:19:20 -0500

    Caching and fixes

--- SolastaUnfinishedBusiness/Models/SpellbookContext.cs
+++ SolastaUnfinishedBusiness/Models/SpellbookContext.cs
@@ -20,11 +20,8 @@ namespace SolastaUnfinishedBusiness.Models;
 internal static class SpellbookContext
 {
     private static bool _initialized;
-    internal readonly static string LOOTED_SPELLBOOK_PREFIX = "LootedSpellbook_";
-    private static Dictionary<string, bool> lootPackDefinitionsProcessed = new Dictionary<string, bool>();
-    private static Dictionary<string, MonsterDefinition> monsterProcessed = new Dictionary<string, MonsterDefinition>();
     private static Dictionary<string, SpellDefinition> wizardSubspellParent = new Dictionary<string, SpellDefinition>();
-    private static Dictionary<string, List<SpellDefinition>> monsterSpells = new Dictionary<string, List<SpellDefinition>>();
+    private static Dictionary<string, List<SpellDefinition>> monsterSpellCache = new Dictionary<string, List<SpellDefinition>>();
 
     private static ItemDefinition _spellbookDefinition = DatabaseHelper.GetDefinition<ItemDefinition>("Spellbook");
 
@@ -36,17 +33,6 @@ internal static class SpellbookContext
         }
 
         _initialized = true;
-
-        AddSpellbookToAllMonsters();
-    }
-
-    internal static void AddSpellbookToAllMonsters()
-    {
-        Main.Info("AddSpellbookToAllMonsters");
-        foreach (var monster in DatabaseRepository.GetDatabase<MonsterDefinition>())
-        {
-            if (monster != null) AddSpellbookToMonster(monster);
-        }
     }
 
     private static void initializeWizardSubspellParents()
@@ -79,17 +65,20 @@ internal static class SpellbookContext
         else return spell;
     }
 
-    internal static List<SpellDefinition> GetWizardSpells(MonsterDefinition monster)
+    internal static List<SpellDefinition> GetWizardSpells(MonsterDefinition monsterDef)
     {
         var result = new List<SpellDefinition>();
 
-        if (monster.features != null)
+        if (monsterDef == null || monsterDef.Name == null) return result;
+        if (monsterSpellCache.ContainsKey(monsterDef.Name)) return monsterSpellCache[monsterDef.Name];
+
+        if (monsterDef.features != null)
         {
-            foreach (var feature in monster.Features)
+            foreach (var feature in monsterDef.Features)
             {
                 if (feature is FeatureDefinitionCastSpell featureCastSpell)
                 {
-                    Main.Info($"* {monster.Name} CastSpell {featureCastSpell.Name} ability {featureCastSpell.SpellcastingAbility} castlevel {featureCastSpell.SpellCastingLevel} maxlevel {featureCastSpell.SpellListDefinition?.MaxSpellLevel}");
+                    Main.Info($"* {monsterDef.Name} CastSpell {featureCastSpell.Name} ability {featureCastSpell.SpellcastingAbility} castlevel {featureCastSpell.SpellCastingLevel} maxlevel {featureCastSpell.SpellListDefinition?.MaxSpellLevel}");
 
                     if (featureCastSpell.SpellListDefinition != null)
                     {
@@ -101,7 +90,7 @@ internal static class SpellbookContext
                                 if (level > 0)
                                 {
                                     var s = GetSpellRoot(spell);
-                                    Main.Info($"> {monster.Name} LVL {level} Spell {s.Name} WIZARD {SpellListDefinitions.SpellListWizard.ContainsSpell(s)}");
+                                    Main.Info($"> {monsterDef.Name} LVL {level} Spell {s.Name} WIZARD {SpellListDefinitions.SpellListWizard.ContainsSpell(s)}");
                                     if (!result.Contains(s) && SpellListDefinitions.SpellListWizard.ContainsSpell(s)) result.Add(s);
                                 }
 
@@ -110,7 +99,7 @@ internal static class SpellbookContext
 
                         foreach (var s in featureCastSpell.SpellListDefinition.SourceSpellLists)
                         {
-                            Main.Info($"! {monster.Name} SourceSpellLists {s.Name}");
+                            Main.Info($"! {monsterDef.Name} SourceSpellLists {s.Name}");
                         }
                     }
                 }
@@ -119,153 +108,54 @@ internal static class SpellbookContext
 
         foreach (var spell in result)
         {
-            Main.Info($"++ {monster.Name} GetWizardSpells {spell.Name}");
+            Main.Info($"++ {monsterDef.Name} GetWizardSpells {spell.Name}");
         }
+
+        monsterSpellCache[monsterDef.Name] = result;
 
         return result;
     }
 
-    internal static void AddSpellbookToMonster(MonsterDefinition monster)
+    internal static RulesetItemSpellbook MakeBlankSpellbook()
     {
-        if (!monster) return;
+        IRulesetItemFactoryService service = ServiceRepository.GetService<IRulesetItemFactoryService>();
+        RulesetItem item = service.CreateStandardItem(_spellbookDefinition);
+        if (item is RulesetItemSpellbook result) return result;
 
-        if (monsterProcessed.ContainsKey(monster.name)) return;
-        monsterProcessed.Add(monster.name, monster);
-
-        Main.Info($"SpellbookContext MONSTER {monster.Name} family {monster.CharacterFamily} lootdef {monster.DroppedLootDefinition?.Name}");
-        var wizardSpellList = GetWizardSpells(monster);
-        if (wizardSpellList.Count == 0) return;
-
-        monsterSpells[monster.Name] = wizardSpellList;
-
-        if (monster.DroppedLootDefinition == null)
-        {
-            Main.Info($"SpellbookContext MONSTER {monster.Name} NEW");
-            var spellbookLootPack = LootPackDefinitionBuilder
-                .Create($"CE_SpellbookLoot_{monster.Name}")
-                .SetGuiPresentationNoContent()
-                .AddExplicitItem(ItemDefinitions._1D6_Gold_Coins)
-                .AddToDB();
-
-            monster.DroppedLootDefinition = spellbookLootPack;
-        }
-
-        if (!lootPackDefinitionsProcessed.ContainsKey(monster.DroppedLootDefinition.Name))
-        {
-            lootPackDefinitionsProcessed.Add(monster.DroppedLootDefinition.Name, true);
-
-            foreach (var spell in wizardSpellList)
-            {
-                Main.Info($"!! AddSpellbookToMonster spell {monster.name} {spell.Name}");
-            }
-
-            
-            var spellbookDef = ItemDefinitionBuilder
-                .Create(_spellbookDefinition, $"{LOOTED_SPELLBOOK_PREFIX}{monster.Name}")
-                .SetGuiPresentation(_spellbookDefinition.GuiPresentation)
-                .MakeMagical()
-                .HideFromDungeonEditor()
-                .AddToDB();
-            
-
-            //var spellbookDef = _spellbookDefinition;
-            //spellbookDef.ItemTags.Add($"{LOOTED_SPELLBOOK_PREFIX}{monster.Name}");
-
-            
-            var guiWrapperService = ServiceRepository.GetService<IGuiWrapperService>();
-            guiWrapperService.AddUserItem(spellbookDef);
-            
-
-            monster.DroppedLootDefinition.ItemOccurencesList.Add(
-                new ItemOccurence
-                {
-                    itemMode = ItemOccurence.SelectionMode.Explicit,
-                    itemDefinition = spellbookDef,
-                    diceNumber = 1,
-                    diceType = RuleDefinitions.DieType.D1,
-                    additiveModifier = 0
-                }
-             );
-        }
+        return null;
     }
 
-    private static string GetMonsterIDFromSpellbook(RulesetItemSpellbook spellbook)
+    internal static void AddSpellbookToDroppedLoot(RulesetCharacterMonster monster)
     {
-        var result = "";
+        if (monster == null || monster.Name == null || monster.MonsterDefinition == null || monster.droppedItems == null) return;
 
-        Main.Info($"GetMonsterNameFromSpellbook");
-        /*
-        foreach(var tag in spellbook.ItemDefinition.ItemTags)
+        Main.Info($"AddSpellbookToDroppedLoot {monster.Name}");
+
+        var spellList = GetWizardSpells(monster.MonsterDefinition);
+        if (spellList.Count > 0)
         {
-            Main.Info($"> GetMonsterNameFromSpellbook tag {tag}");
-            if (tag.Contains(LOOTED_SPELLBOOK_PREFIX))
+            foreach (var droppedItem in monster.droppedItems)
             {
-                result = tag.Replace(LOOTED_SPELLBOOK_PREFIX, "");
-                break;
-            }
-        }
-        */
-
-        if (spellbook.ItemDefinition.Name.Contains(LOOTED_SPELLBOOK_PREFIX)) result = spellbook.ItemDefinition.Name.ReplaceFirst(LOOTED_SPELLBOOK_PREFIX, "");
-
-
-        return result;
-    }
-
-    internal static void LootGroundScribeSpellbooks(CharacterActionLootGround action)
-    {
-        var groundItems = new Dictionary<RulesetItem, TA.int3>();
-
-        var itemService = ServiceRepository.GetService<IGameLocationItemService>();
-        var gameLocationCharacter = action.ActingCharacter;
-
-        if (itemService != null && gameLocationCharacter != null) itemService.EnumerateGroundItemsAroundCharacter(action.ActingCharacter, 10, groundItems);
-
-        foreach (var entry in groundItems)
-        {
-            var item = entry.Key;
-
-            Main.Info($"LootGroundScribeSpellbooks {item.Name} owner {item.OwnerName}");
-
-            if (item is RulesetItemSpellbook spellbook)
-            {
-                if (spellbook.ScribedSpells == null || spellbook.ScribedSpells.Count == 0)
+                if (droppedItem is RulesetItemSpellbook droppedSpellbook)
                 {
-                    //var monsterName = spellbook.ItemDefinition.Name.ReplaceFirst("Spellbook_", "");
-                    var monsterName = GetMonsterIDFromSpellbook(spellbook);
-                    if (monsterName != null && monsterName != "" && monsterProcessed.ContainsKey(monsterName))
+                    if (droppedSpellbook.ScribedSpells.Count > 0)
                     {
-                        var monster = monsterProcessed[monsterName];
-                        spellbook.ItemDefinition = _spellbookDefinition;
-                        spellbook.ScribedSpells = GenerateSpellList(monster);
-                        spellbook.OwnerName = monsterName.Replace("_", " ");
+                        Main.Info($"AddSpellbookToDroppedLoot {monster.Name} SKIPPED");
+                        return;
                     }
                 }
             }
+
+            var spellbook = MakeBlankSpellbook();
+            if (spellbook != null)
+            {
+                spellbook.ScribedSpells = spellList;
+                spellbook.OwnerName = monster.Name;
+
+                monster.droppedItems.Add(spellbook);
+                Main.Info($"AddSpellbookToDroppedLoot {monster.Name} ADDED");
+            }
         }
     }
 
-    internal static List<SpellDefinition> GenerateSpellList(MonsterDefinition monster)
-    {
-        var result = new List<SpellDefinition>();
-
-        if (monsterSpells.ContainsKey(monster.Name)) result = monsterSpells[monster.Name];
-        else result.Add(SpellDefinitions.MagicMissile);
-
-        foreach (var spell in result)
-        {
-            Main.Info($"++ {monster.Name} GenerateSpellList {spell.Name}");
-        }
-
-        return result;
-    }
-
-    internal static List<SpellDefinition> GenerateSpellList(int maxSpellLevel = 1)
-    {
-        var result = new List<SpellDefinition>();
-
-        result.Add(SpellDefinitions.CharmPerson);
-
-        return result;
-    }
 }
